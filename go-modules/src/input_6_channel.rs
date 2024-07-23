c6294d762a523dc45c50e0987f2411b270ba04cd
SpieringsAE <maud_spierings@hotmail.com>
2024-07-23 09:23:32 +0200

    changed configuration serialization, fixed supplies, added other channel functions

--- go-modules/src/input_6_channel.rs
+++ go-modules/src/input_6_channel.rs
@@ -13,9 +13,32 @@ const RESISTORMATRIX: [u8; 4] = [0, 3, 1, 2];
 
 #[repr(u8)]
 #[derive(Clone, Copy)]
+/// Selects the funcion of a given input channel, some functions require two channels
 pub enum InputModule6ChannelFunc {
+    /// Get the raw adc value, u16 is the number of samples
     AnalogRaw(u16) = 1,
+    /// Get the voltage in mV, u16 is the number of samples
     AnalogmV(u16),
+    /// Read a digital signal, high or low
+    Digital,
+    /// Measure the frequency of a signal
+    Frequency,
+    /// Measure the low period of a duty cycle signal
+    DutyLow,
+    /// Measure the high period of a duty cycle signal
+    DutyHigh,
+    /// RPM mode, u8 is the number of pulses per rotation
+    /// Combined with the adjacent channel 1&2, 3&4, 5&6
+    RPM(u8),
+    /// Count pulses on this channel
+    /// Combined with the adjacent channel 1&2, 3&4, 5&6
+    PulseCounter,
+}
+
+impl InputModule6ChannelFunc {
+    fn discriminant(&self) -> u8 {
+        unsafe { *<*const _>::from(self).cast::<u8>() }
+    }
 }
 
 #[derive(Clone, Copy)]
@@ -34,6 +57,7 @@ pub enum InputModule6ChannelPullDown {
     PD10k,
 }
 
+#[repr(u8)]
 #[derive(Clone, Copy)]
 pub enum InputModule6ChannelVoltage {
     Voltage5V,
@@ -60,11 +84,12 @@ pub struct InputModule6ChannelValues {
     pub channel6: u32,
 }
 
+#[repr(u8)]
 #[derive(Default, Clone, Copy)]
 pub enum InputModule6ChannelSupply {
-    Off,
     #[default]
-    On,
+    On = 1,
+    Off,
 }
 
 #[derive(Default)]
@@ -170,20 +195,25 @@ impl Default for InputModule6ChannelChannel {
     }
 }
 
-impl InputModule6ChannelChannel {
-    fn serialize(&self, data: &mut [u8]) {
-        let (func, samples) = match self.func {
-            InputModule6ChannelFunc::AnalogRaw(samples) => (1, samples),
-            InputModule6ChannelFunc::AnalogmV(samples) => (2, samples),
-        };
-        data[0] = func;
-        data[1] = RESISTORMATRIX[self.pu as usize]
-            | RESISTORMATRIX[self.pd as usize] << 2
-            | (self.volt as u8) << 6;
-        data[2] = (samples >> 8) as u8;
-        data[3] = samples as u8;
-        data[4] = 0;
-        data[5] = 0;
+impl InputModule6ChannelConfiguration {
+    fn serialize(&self, tx: &mut [u8]) {
+        for (i, channel) in self.channels.iter().enumerate() {
+            let samples = match channel.func {
+                InputModule6ChannelFunc::AnalogRaw(samples) => samples.clamp(0, 1000),
+                InputModule6ChannelFunc::AnalogmV(samples) => samples.clamp(0, 1000),
+                InputModule6ChannelFunc::RPM(pulses) => (pulses.clamp(0, 200) as u16) << 8,
+                _ => 0,
+            };
+            tx[6 + i * 6] = channel.func.discriminant();
+            tx[7 + i * 6] = RESISTORMATRIX[channel.pu as usize]
+                | RESISTORMATRIX[channel.pd as usize] << 2
+                | (channel.volt as u8) << 6;
+            tx[8 + i * 6] = (samples >> 8) as u8;
+            tx[9 + i * 6] = samples as u8;
+            tx[10 + i * 6] = 0;
+            tx[11 + i * 6] = 0;
+            tx[42 + i / 2] = self.supplies[i / 2] as u8;
+        }
     }
 }
 
@@ -267,12 +297,7 @@ where
             return Err((module.module.degrade(), module.configuration));
         }
         let mut tx = [0u8; INPUTMODULE6CHANNELMESSAGELENGTH + 5];
-        for (i, channel) in module.configuration.channels.iter().enumerate() {
-            channel.serialize(&mut tx[6 + i * 6..12 + i * 6]);
-        }
-        for (i, supply) in module.configuration.supplies.iter().enumerate() {
-            tx[i + 42] = *supply as u8;
-        }
+        module.configuration.serialize(&mut tx);
         if module
             .module
             .send_spi(
